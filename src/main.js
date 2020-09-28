06df10a25bc137b1003d1e708701c303ca99f65b
Marcus Sera <m.yoshiosera@gmail.com>
2020-09-27 17:51:22 -0700

    deleted excess code

--- src/main.js
+++ src/main.js
@@ -4,17 +4,6 @@ import App from './App';
 import router from './router';
 import store from './store';
 
-// import AuthHanlder from './components/AuthHandler';
-
-// const app = createApp(App);
-
-//  Vue.use(VueRouter) where does this go
-
-// const router = new VueRouter({
-//   mode: 'history',
-//   routes: [{ path: '/oauth2/callback', component: AuthHanlder }],
-// });
-
 createApp(App)
   .use(store)
   .use(router)
